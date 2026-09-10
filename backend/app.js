18b88fa0b047b823260fddf83cfc02de2b4daf45
souj05 <souj05@users.noreply.github.com>
2026-09-10 18:30:29 +0530

    done

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch((err) => {
+    console.log("Connection failed!",err.message);
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("images")));
+
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
+
+app.listen(port, ()=>{
+  console.log("port working @"+port)
+})
+
+module.exports = app;
